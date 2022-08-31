fca10d26beb9835ed70c6283e3171e93167bb7ca
ÐFarma20 <kustov.ius@students.dvfu.ru>
2022-08-31 11:57:29 +1000

    Реализован паралельный таймер у класса NewUser, добавлен словарь с новыми пользователями чата

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import os
 import telebot
+import threading
 from newUserClass import NewUser
 from dotenv import load_dotenv
 
 
-#Подключаем переменные окружения
+# Подключаем переменные окружения
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
+
+# Словарь новых пользователей чатов писок всех новых пользователей
+newUserList = {}
+
+
 
 
 @bot.message_handler(commands=['help'])
@@ -21,14 +27,34 @@ def helps(message):
                                       '/deleteEntryMessages — удалять сообщения о входе пользователей в чат\n'
                                       '/greeting — встречать прошедших проверку пользователей сообщением\n'
                                       '/noAttack — отключить SpamBOT\n'
-                                      '/noLinks — автоматически удалять сообщения со ссылками\n')
+                                      '/noLinks — автоматически удалять сообщения со ссылками\n'
+                                      '/enter - эмитация входа пользователя\n'
+                                      '/getUsers - возвращает список новых пользователей\n'
+                                      '/start - для запуска бота')
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    user = NewUser(message.from_user.id)
-    bot.send_message(message.chat.id, user.get_user_id())
-    bot.send_message(message.chat.id, message.chat.id)
+    newUserList[message.chat.id] = []
+
+
+
+@bot.message_handler(commands=['enter'])
+def enter(message):
+    bot.send_message(message.chat.id, f'Здравствуй {message.from_user.username}!'
+                                      f' Чтобы оставаться в чате и отправлять сообщения'
+                                      f' тебе необходимо пройти проверку на робота')
+    user = NewUser(message.from_user.id, message.chat.id)
+    newUserList[message.chat.id].append(user)
+
+    # Паралельно запускаем таймер у каждого нового пользователя
+    threading.Thread(target=user.timer).start()
+
+
+
+@bot.message_handler(commands=['getUsers'])
+def get_users(message):
+    bot.send_message(message.chat.id, f'{newUserList}')
 
 
 bot.infinity_polling()
--- newUserClass.py
+++ newUserClass.py
@@ -1,15 +1,31 @@
-from accessify import private
+import time
 
 
 class NewUser(object):
-    def __init__(self, id):
-        self.__userID = id
+    def __init__(self, user_id, chat_id):
+        self.__userID = user_id
+        self.__chatID = chat_id
         self.__time = 60
+        self.__captcha_is_done = False
+        self.__is_kick = False
 
     def get_user_id(self):
         return self.__userID
 
-    @staticmethod
+    def get_chat_id(self):
+        return self.__chatID
+
     def timer(self):
-        print('Coming soon')
-        pass
+        print('Таймер запущен')
+
+        timing_start = time.time()
+
+        while True:
+            if self.__captcha_is_done:
+                print('Капча пройдена')
+                break
+
+            if time.time() - timing_start >= self.__time:
+                print('Пользователь кикнут')
+                break
+
